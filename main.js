7b05acb6c91f04f5d331b85ff1869306c23dc8db
Factory_MicheleOrazi <michele@factorysnc.com>
2018-09-24 14:53:17 +0200

    Update 1.0.9

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ var template = [{
     }
   ]
 }];
-// Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
